82d56f94ee5eeca6070837f26ec7133ea2f4c07f
skubidubudu1 <wojtek.adamczyk99@gmail.com>
2023-03-09 18:33:07 +0100

    Dodaj pliki projektów.

---
+++ ToDoApp/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+
+namespace ToDoApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> ToDos = new List<string>();
+
+            string message = "What would you like to do? (Load/Add/Delete/Display/Clear/Quit)";
+            Console.WriteLine(message);
+            string decision = Console.ReadLine();
+
+            while (decision != "Quit")
+            {
+                switch (decision)
+                {
+                    case "Add":
+                        {
+                            string ToList = Console.ReadLine();
+                            ToDos.Add(ToList);
+                            Console.WriteLine($"{ToList} added to the list. \n{message}");
+                            decision = Console.ReadLine();
+                        }
+                        break;
+
+                    case "Display":
+                        {
+                            if (ToDos.Count > 0)
+                            {
+                                Console.WriteLine("Currently on the list:");
+
+                                foreach (string str in ToDos)
+                                    Console.WriteLine(str);
+                            }
+                            else
+                                Console.WriteLine("List is empty.");
+                            
+                            Console.WriteLine(message);
+                            decision = Console.ReadLine();
+                        }
+                        break;
+
+                    case "Delete":
+                        {
+                            string fromList = Console.ReadLine();
+                            bool removed = ToDos.Remove(fromList);
+
+                            if (removed)
+                                ToDos.Remove(fromList);
+                            else
+                                Console.WriteLine($"{fromList} was not found on the list.");
+
+                            Console.WriteLine($"{fromList} removed from the list. \n{message}");
+                            decision = Console.ReadLine();
+                        }
+                        break;
+
+                    case "Save":
+                        {
+                            State.Save(ToDos);
+
+                            Console.WriteLine($"List succesfully saved! {message}");
+                            decision = Console.ReadLine();
+                        }
+                        break;
+
+                    case "Load":
+                        {
+                            ToDos = State.Load();
+
+                            Console.WriteLine($"List succesfully loaded! {message}");
+                            decision = Console.ReadLine();
+                        }
+                        break;
+
+                    case "Clear":
+                        {
+                            ToDos.Clear();
+
+                            Console.WriteLine($"List cleared. {message}");
+                            decision = Console.ReadLine();
+                        }
+                        break;
+
+                    default:
+                        {
+                            Console.WriteLine($"Unknown command. \n{message}");
+                            decision = Console.ReadLine();
+                        }
+                        break;               
+                }             
+            }          
+        }
+    }
+}
---
+++ ToDoApp/State.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp
+{
+    public class State
+    {
+        private static string filePath = "todos.txt";
+
+        public static void Save(List<string> ToDos)
+        {
+            using (StreamWriter sw = new StreamWriter(filePath))
+            {
+                foreach (string s in ToDos)
+                    sw.WriteLine(s);
+            }
+        }
+
+        public static List<string> Load()
+        {
+            List<string> ToDos = new List<string>();
+
+            using(StreamReader sr = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    ToDos.Add(line);
+                }
+                return ToDos;
+            }
+        }
+    }
+}
